20e7d11c4f4b0a9a618941e246ca833635a4f0e6
Lucanatorr <45624810+Lucanatorr@users.noreply.github.com>
2020-07-01 14:46:39 -0400

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,18 @@
+Locales['en'] = {
+    ['blip_name'] = 'pharmacy',
+    ['open_pharm'] = 'press ~g~E~s~ to talk to the doctor',
+    ['help_text'] = 'press ~INPUT_CONTEXT~ to talk to the doctor',
+    ['shop_item'] = '$%s',
+    ['shop_title'] = 'pharmacy',
+    ['bought'] = 'You have purchased ~b~%sx~s~ ~r~%s~s~ for ~g~$%s~s~',
+    ['not_enough_space'] = 'not enough free space in your inventory',
+    ['not_enough_cash'] = 'you need ~g~$%s~s~ more to purchase',
+    ['shop_confirm'] = 'buy %sx %s for $%s?',
+    ['no'] = 'no',
+    ['yes'] = 'yes',
+    ['used_painkiller'] = 'you feel ~g~slightly better~s~',
+    ['max_refills_met'] = 'you have reached the ~r~max number~s~ of refills',
+    ['no_pres'] = 'you do not have the right Prescription',
+    ['come_back_later'] = 'the Pharmacy is ~r~closed~s~, come back later.',
+    ['given_discount'] = 'you have been given a 20 percent discount'
+}
